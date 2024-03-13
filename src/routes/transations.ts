e8f8b1a09f38abd06e4176563200ad3c2d513309
Joao Kicunga <kicungati@gmail.com>
2024-03-13 09:26:57 +0100

    Plugins do Fastify.

--- src/routes/transations.ts
+++ src/routes/transations.ts
@@ -1,6 +1,7 @@
+import { FastifyInstance } from "fastify";
 import { knex } from "../database";
 
-export async function transationsRoutes(app){
+export async function transationsRoutes(app: FastifyInstance){
 	app.get("/hello", async () => {
 		const transtactions = await knex("transations")
 			.where("amount", 1000)
